665b06c010b96968be3dfa86a05e27f53aaca646
nerd-sourav <souravprasad1869@gmail.com>
2022-03-05 17:30:25 +0530

    changes made

---
+++ lib/responsive/responsive_layout.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/cupertino.dart';
+
+class ResponsiveLayout extends StatelessWidget {
+  const ResponsiveLayout({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return LayoutBuilder();
+  }
+}
