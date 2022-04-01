650eb52169fcdfd4321716d6380d7e9390413f80
nerd-sourav <souravprasad1869@gmail.com>
2022-04-01 14:18:03 +0530

    changes made

--- lib/main.dart
+++ lib/main.dart
@@ -5,7 +5,9 @@ import 'package:watsapp_clone/screens/mobile_screen_layout.dart';
 import 'package:watsapp_clone/screens/web_screen_layout.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(
+    const MyApp(),
+  );
 }
 
 class MyApp extends StatelessWidget {
--- lib/screens/mobile_screen_layout.dart
+++ lib/screens/mobile_screen_layout.dart
@@ -11,72 +11,73 @@ class MobileScreenLayout extends StatelessWidget {
     return DefaultTabController(
       length: 3,
       child: Scaffold(
-          appBar: AppBar(
-            backgroundColor: appBarColor,
-            title: const Text(
-              'WhatsApp',
-              style: TextStyle(
-                fontSize: 20,
-                color: Colors.grey,
-                fontWeight: FontWeight.bold,
-              ),
+        appBar: AppBar(
+          backgroundColor: appBarColor,
+          title: const Text(
+            'WhatsApp',
+            style: TextStyle(
+              fontSize: 20,
+              color: Colors.grey,
+              fontWeight: FontWeight.bold,
             ),
-            centerTitle: false,
-            actions: [
-              IconButton(
+          ),
+          centerTitle: false,
+          actions: [
+            IconButton(
+              onPressed: () => {},
+              icon: const Icon(CupertinoIcons.search, color: Colors.grey),
+            ),
+            IconButton(
                 onPressed: () => {},
-                icon: const Icon(CupertinoIcons.search, color: Colors.grey),
-              ),
-              IconButton(
-                  onPressed: () => {},
-                  icon: const Icon(CupertinoIcons.ellipsis_vertical,
-                      color: Colors.grey))
+                icon: const Icon(CupertinoIcons.ellipsis_vertical,
+                    color: Colors.grey))
+          ],
+          bottom: const TabBar(
+            labelPadding: EdgeInsets.all(10),
+            indicatorColor: tabColor,
+            indicatorWeight: 4,
+            labelColor: tabColor,
+            unselectedLabelColor: Colors.grey,
+            labelStyle: TextStyle(fontWeight: FontWeight.bold),
+            tabs: [
+              Text('CHATS'),
+              Text('STATUS'),
+              Text('CALLS'),
             ],
-            bottom: const TabBar(
-              labelPadding: EdgeInsets.all(10),
-              indicatorColor: tabColor,
-              indicatorWeight: 4,
-              labelColor: tabColor,
-              unselectedLabelColor: Colors.grey,
-              labelStyle: TextStyle(fontWeight: FontWeight.bold),
-              tabs: [
-                Text('CHATS'),
-                Text('STATUS'),
-                Text('CALLS'),
-              ],
-            ),
           ),
-          body: Padding(
-            padding: const EdgeInsets.all(10),
-            child: ListView.builder(
-              itemBuilder: ((context, index) {
-                return Padding(
-                  padding: const EdgeInsets.only(bottom: 8.0),
-                  child: ListTile(
-                    leading: CircleAvatar(
-                      backgroundImage:
-                          NetworkImage(info[index]['profilePic'].toString()),
-                    ),
-                    title: Text(info[index]['name'].toString()),
-                    subtitle: Padding(
-                      padding: const EdgeInsets.only(top: 8.0),
-                      child: Text(info[index]['message'].toString()),
-                    ),
-                    trailing: Text(
-                      info[index]['time'].toString(),
-                      style: const TextStyle(fontSize: 13, color: Colors.grey),
-                    ),
+        ),
+        body: Padding(
+          padding: const EdgeInsets.all(10),
+          child: ListView.builder(
+            itemBuilder: ((context, index) {
+              return Padding(
+                padding: const EdgeInsets.only(bottom: 8.0),
+                child: ListTile(
+                  leading: CircleAvatar(
+                    backgroundImage:
+                        NetworkImage(info[index]['profilePic'].toString()),
                   ),
-                );
-              }),
-              itemCount: info.length,
-            ),
+                  title: Text(info[index]['name'].toString()),
+                  subtitle: Padding(
+                    padding: const EdgeInsets.only(top: 8.0),
+                    child: Text(info[index]['message'].toString()),
+                  ),
+                  trailing: Text(
+                    info[index]['time'].toString(),
+                    style: const TextStyle(fontSize: 13, color: Colors.grey),
+                  ),
+                ),
+              );
+            }),
+            itemCount: info.length,
           ),
-          floatingActionButton: const CircleAvatar(
-            radius: 25,
-            child: Icon(Icons.message_outlined),
-            backgroundColor: messageColor,
-          )),
+        ),
+        floatingActionButton: const CircleAvatar(
+          radius: 25,
+          child: Icon(Icons.message_outlined),
+          backgroundColor: messageColor,
+        ),
+      ),
     );
   }
 }
