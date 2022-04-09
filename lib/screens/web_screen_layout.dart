ec1195e009e1b53620e305bf14629a6f34f3e7ee
nerd-sourav <souravprasad1869@gmail.com>
2022-04-09 12:25:16 +0530

    few more changes made

--- lib/screens/web_screen_layout.dart
+++ lib/screens/web_screen_layout.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:watsapp_clone/widgets/contact_list.dart';
 
 class WebScreenLayout extends StatelessWidget {
   const WebScreenLayout({Key? key}) : super(key: key);
@@ -15,6 +16,30 @@ class WebScreenLayout extends StatelessWidget {
             fontWeight: FontWeight.bold,
           ),
         ),
+      ),
+      body: Row(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Expanded(
+            child: SingleChildScrollView(
+              child: Column(children: const [
+                //web profile bar
+                // web search bar
+                ContactList(),
+              ]),
+            ),
+          ),
+          Container(
+            width: MediaQuery.of(context).size.width * (0.75),
+            height: double.infinity,
+            decoration: const BoxDecoration(
+              image: DecorationImage(
+                fit: BoxFit.cover,
+                image: AssetImage('assets/backgroundImage.png'),
+              ),
+            ),
+          )
+        ],
       ),
     );
   }
